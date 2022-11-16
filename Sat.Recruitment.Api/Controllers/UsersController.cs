fa3d262c67858824f31b97ef7afdf80624866663
LuisBurgosV <luis.burgos.vidales@gmail.com>
2022-11-16 13:23:16 +0100

    ReadUser method

--- Sat.Recruitment.Api/Controllers/UsersController.cs
+++ Sat.Recruitment.Api/Controllers/UsersController.cs
@@ -35,7 +35,6 @@ namespace Sat.Recruitment.Api.Controllers
                 return NoneSuccessfulResult("Failed to normalize email");
             }
             
-
             var newUser = new User
             {
                 Name = name,
@@ -47,25 +46,10 @@ namespace Sat.Recruitment.Api.Controllers
             };
 
             UsersFactory.NewUser(ref newUser);
+
             try
             {
-                var reader = UsersFactory.ReadUsersFromFile();
-
-                while (reader.Peek() >= 0)
-                {
-                    var line = reader.ReadLineAsync().Result;
-                    var user = new User
-                    {
-                        Name = line.Split(',')[0].ToString(),
-                        Email = line.Split(',')[1].ToString(),
-                        Phone = line.Split(',')[2].ToString(),
-                        Address = line.Split(',')[3].ToString(),
-                        UserType = line.Split(',')[4].ToString(),
-                        Money = decimal.Parse(line.Split(',')[5].ToString()),
-                    };
-                    _users.Add(user);
-                }
-                reader.Close();
+                ReadUsers();              
             }
             catch (Exception ex)
             {
@@ -79,6 +63,26 @@ namespace Sat.Recruitment.Api.Controllers
                 return SuccessfulResult("User Created");
         }
 
+        private void ReadUsers()
+        {
+            var reader = UsersFactory.ReadUsersFromFile();
+
+            while (reader.Peek() >= 0)
+            {
+                var line = reader.ReadLineAsync().Result;
+                var user = new User
+                {
+                    Name = line.Split(',')[0].ToString(),
+                    Email = line.Split(',')[1].ToString(),
+                    Phone = line.Split(',')[2].ToString(),
+                    Address = line.Split(',')[3].ToString(),
+                    UserType = line.Split(',')[4].ToString(),
+                    Money = decimal.Parse(line.Split(',')[5].ToString()),
+                };
+                _users.Add(user);
+            }
+            reader.Close();
+        }
 
         //Validate errors
         private string ValidateErrors(string name, string email, string address, string phone)
