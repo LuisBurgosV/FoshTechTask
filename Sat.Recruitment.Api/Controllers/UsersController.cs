dbd6750f23ef10200b6a5825d6e653ca7a8b140d
LuisBurgosV <luis.burgos.vidales@gmail.com>
2022-11-16 13:00:12 +0100

    commit solution

---
+++ Sat.Recruitment.Api/Controllers/UsersController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sat.Recruitment.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace Sat.Recruitment.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public partial class UsersController : ControllerBase
+    {
+        private readonly List<User> _users = new List<User>();
+
+        [HttpPost]
+        [Route("/create-user")]
+        public async Task<Result> CreateUser(string name, string email, string address, string phone, string userType, string money)
+        {
+            var errors = ValidateErrors(name, email, address, phone);
+
+            if (errors != String.Empty)
+                return NoneSuccessfulResult(errors);
+
+            try
+            {
+                email = UsersFactory.NormalizeEmail(email);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return NoneSuccessfulResult("Failed to normalize email");
+            }
+            
+
+            var newUser = new User
+            {
+                Name = name,
+                Email = email,
+                Address = address,
+                Phone = phone,
+                UserType = userType,
+                Money = decimal.Parse(money)
+            };
+
+            UsersFactory.NewUser(ref newUser);
+            try
+            {
+                var reader = UsersFactory.ReadUsersFromFile();
+
+                while (reader.Peek() >= 0)
+                {
+                    var line = reader.ReadLineAsync().Result;
+                    var user = new User
+                    {
+                        Name = line.Split(',')[0].ToString(),
+                        Email = line.Split(',')[1].ToString(),
+                        Phone = line.Split(',')[2].ToString(),
+                        Address = line.Split(',')[3].ToString(),
+                        UserType = line.Split(',')[4].ToString(),
+                        Money = decimal.Parse(line.Split(',')[5].ToString()),
+                    };
+                    _users.Add(user);
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return NoneSuccessfulResult("Failure to read file");
+            }
+
+            if (UsersFactory.CheckDuplicatedUsers(_users, newUser))
+                return NoneSuccessfulResult("The user is duplicated");
+            else
+                return SuccessfulResult("User Created");
+        }
+
+
+        //Validate errors
+        private string ValidateErrors(string name, string email, string address, string phone)
+        {
+            string errors = String.Empty;
+
+            if (name == null)
+                //Validate if Name is null
+                errors = "The name is required";
+            if (email == null)
+                //Validate if Email is null
+                errors += " The email is required";
+            if (address == null)
+                //Validate if Address is null
+                errors += " The address is required";
+            if (phone == null)
+                //Validate if Phone is null
+                errors += " The phone is required";
+
+            return errors;
+        }
+
+        private Result SuccessfulResult(string message)
+        {
+            return new Result()
+            {
+                IsSuccess = true,
+                Errors = message
+            };
+        }
+
+        private Result NoneSuccessfulResult(string message)
+        {
+            return new Result()
+            {
+                IsSuccess = false,
+                Errors = message
+            };
+        }
+    }
+}
---
+++ Sat.Recruitment.Api/Entities/UsersFactory.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Policy;
+
+namespace Sat.Recruitment.Api.Entities
+{
+    public class UsersFactory
+    {
+        private const string Normal = "Normal";
+        private const string SuperUser = "SuperUser";
+        private const string Premium = "Premium";
+
+        public static bool CheckDuplicatedUsers(List<User> _users, User newUser)
+        {
+            bool isDuplicated = false;
+
+            foreach (var user in _users)
+            {
+                if (user.Email == newUser.Email || user.Phone == newUser.Phone || user.Name == newUser.Name || user.Address == newUser.Address)
+                    isDuplicated = true;
+                else isDuplicated = false;
+            }
+            return isDuplicated;
+        }
+
+        public static void NewUser(ref User newUser)
+        {
+            switch (newUser.UserType)
+            {
+                case Normal:
+
+                    if (newUser.Money > 100)
+                    {
+                        CalculateMoneyGif(ref newUser, 0.12);
+                    }
+                    else if (newUser.Money < 100 && newUser.Money > 10)
+                    {
+                        CalculateMoneyGif(ref newUser, 0.8);
+                    }
+                    break;
+                case SuperUser:
+
+                    if (newUser.Money > 100)
+                    {
+                        CalculateMoneyGif(ref newUser, 0.20);
+                    }
+                    break;
+                case Premium:
+
+                    if (newUser.Money > 100)
+                    {
+                        var gif = newUser.Money * 2;
+                        newUser.Money = newUser.Money + gif;
+                    }
+                    break;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        private static void CalculateMoneyGif(ref User newUser, double decimalPercentage)
+        {
+            var percentage = Convert.ToDecimal(decimalPercentage);
+            var gif = newUser.Money * percentage;
+            newUser.Money = newUser.Money + gif;
+        }
+
+        public static string NormalizeEmail(string email)
+        {
+            var aux = email.Split(new char[] { '@' }, StringSplitOptions.RemoveEmptyEntries);
+            var atIndex = aux[0].IndexOf("+", StringComparison.Ordinal);
+            aux[0] = atIndex < 0 ? aux[0].Replace(".", "") : aux[0].Replace(".", "").Remove(atIndex);
+
+            return string.Join("@", new string[] { aux[0], aux[1] });
+        }
+
+        public static StreamReader ReadUsersFromFile()
+        {
+            var path = Directory.GetCurrentDirectory() + "/Files/Users.txt";
+            FileStream fileStream = new FileStream(path, FileMode.Open);
+            StreamReader reader = new StreamReader(fileStream);
+
+            return reader;
+        }
+    }
+}
